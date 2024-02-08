c59c41c21869990cfd5a2f184ecf61acb4112e27
PaalKia <147259226+PaalKia@users.noreply.github.com>
2024-02-08 11:47:28 +0100

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -10,8 +10,8 @@ cur = connection.cursor()
 cur.execute("INSERT INTO clients (nom, prenom, message) VALUES (?, ?, ?)",('DUPONT', 'Emilie', 'Je veux te recruter'))
 cur.execute("INSERT INTO clients (nom, prenom, message) VALUES (?, ?, ?)",('PaalKia', 'LeBoss', 'Help'))
 cur.execute("INSERT INTO clients (nom, prenom, message) VALUES (?, ?, ?)",('The', 'Grinch', 'SOS'))
-cur.execute("INSERT INTO clients (nom, prenom, message) VALUES (?, ?, ?)",('SHE/HER', 'LomeBronzé', 'Pourquoi moi ?'))
-cur.execute("INSERT INTO clients (nom, prenom, message) VALUES (?, ?, ?)",('M.', 'Farceur', 'Salut c'est moi monisuer farceur !'))
+cur.execute("INSERT INTO clients (nom, prenom, message) VALUES (?, ?, ?)",('SHE/HER', 'LomeBronzé', 'Pourquoi moi'))
+cur.execute("INSERT INTO clients (nom, prenom, message) VALUES (?, ?, ?)",('M.', 'Farceur', 'Salut c est moi monisuer farceur !'))
 
 
 
