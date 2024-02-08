1da76cd6a735481bebd74214d33c953766f8c5d1
PaalKia <147259226+PaalKia@users.noreply.github.com>
2024-02-08 12:31:16 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template, request, redirect
+from flask import *
 import hashlib
 import sqlite3
 
-app = Flask(__name__) #creating flask app name
+app = Flask(__name__)
 
-@app.route('/')
+@app.route('/resume_2')
 def home():
     return render_template("index.html")
+
+@app.route('/remplirDon')
+def resume_1():
+    return render_template("addFi.html")
+
+@app.route('/')
+def resume_2():
+    return render_template("resume_2.html")
+
+def sha256_hash(input_string):
+    sha256 = hashlib.sha256()
+    sha256.update(input_string.encode('utf-8'))
+    return sha256.hexdigest()
+
+def check_sum_256(array, api_value, key_to_check):
+    array_sum = str(array[0]) + str(api_value)
+    hashed_sum = sha256_hash(str(array_sum))
+    return hashed_sum == key_to_check
 
 @app.route('/consultation/<int:api_key>')
 def afficheListe(api_key):
@@ -20,13 +38,31 @@ def afficheListe(api_key):
         return render_template('read_data.html', data=data)
     return "api_key !!"
 
-@app.route('/fiche_client/<int:post_id>')
-def Readfiche(post_id):
+@app.route('/graphique/<int:api_key>')
+def afficheGraph(api_key):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
+    cursor.execute('SELECT * FROM clients')
     data = cursor.fetchall()
     conn.close()
+    if api_key == 5625719273:
+        return render_template('graphique.html', data=data)
+    return "api_key !!"
+
+@app.route('/ajouter_message', methods=['POST'])
+def ajouter_client():
+    nom = request.form['nom']
+    prenom = request.form['prenom']
+    messages = request.form['messages']
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO clients (nom, prenom, messages) VALUES (?, ?, ?)", (nom, prenom, messages))
+    conn.commit()
+    conn.close()
+
+    return redirect('/resume_2')
+
 
 @app.route('/paris/<int:api_key>')
 def meteo(api_key):
@@ -39,16 +75,11 @@ def meteo(api_key):
         return jsonify(results=data)
     return "api_key !!"
 
-@app.route('/graphique/<int:api_key>')
-def afficheGraph(api_key):
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients')
-    data = cursor.fetchall()
-    conn.close()
-    if api_key == 5625719273:
-        return render_template('graphique.html', data=data)
-    return "api_key !!"
 
-if(__name__ == "__main__"):
-    app.run() (modifié)
+
+@app.route('/resume_template')
+def resume_template():
+    return render_template("resume_template.html")
+
+if __name__ == "__main__":
+    app.run()
